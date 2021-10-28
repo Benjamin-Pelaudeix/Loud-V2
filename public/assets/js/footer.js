3f4d5a62c2c65d485d6dafff422d9f2e68b34555
Benjamin PELAUDEIX <ben.pelau@hotmail.fr>
2021-10-28 10:27:50 +0200

    v2 - header & nav

--- public/assets/js/footer.js
+++
@@ -1,7 +0,0 @@
-// variables
-// generate today date
-let today = new Date()
-// html span where year will be displayed
-let footerDateSpan = document.querySelector('#current-year')
-
-footerDateSpan.textContent = today.getFullYear().toString()
