8e1253198af9f8d725a66e57836791006c4cc17c
Benjamin PELAUDEIX <ben.pelau@hotmail.fr>
2021-11-02 13:44:51 +0100

    v2 - nav update

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -5,6 +5,7 @@
 // variables
 const icon = document.getElementsByClassName('header_menu_icon')[0]
 const nav = document.getElementsByClassName('nav')[0]
+const headerBrand = document.getElementsByClassName('header_brand')[0]
 
 icon.addEventListener('click', () => {
     if (icon.classList.contains('fa-bars')) {
@@ -15,9 +16,13 @@ icon.addEventListener('click', () => {
             nav.style.left = '-100vw'
             icon.classList.toggle('fa-times')
         }
+        headerBrand.style.color = 'var(--main-red-color)';
+        icon.style.color = 'var(--main-red-color)'
     }
     if (icon.classList.contains('fa-times')) {
         nav.style.left = '0'
         icon.classList.toggle('fa-bars')
+        headerBrand.style.color = 'white';
+        icon.style.color = 'white'
     }
 })
