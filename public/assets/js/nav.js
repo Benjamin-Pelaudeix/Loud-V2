f09d5a9c542141f903be2d3cc6eec9db7d3ab38d
Benjamin PELAUDEIX <ben.pelau@hotmail.fr>
2021-11-08 10:09:49 +0100

    v2 - fix nav weird behaviour

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -6,7 +6,6 @@
 const icon = document.getElementsByClassName('header_menu_icon')[0]
 const nav = document.getElementsByClassName('nav')[0]
 const headerBrand = document.getElementsByClassName('header_brand')[0]
-const header = document.getElementsByTagName('header')[0]
 
 icon.addEventListener('click', () => {
     if (icon.classList.contains('fa-bars')) {
@@ -19,13 +18,11 @@ icon.addEventListener('click', () => {
         }
         headerBrand.style.color = 'var(--main-red-color)'
         icon.style.color = 'var(--main-red-color)'
-        header.style.position = 'block'
     }
     if (icon.classList.contains('fa-times')) {
         nav.style.left = '0'
         icon.classList.toggle('fa-bars')
         headerBrand.style.color = 'white'
-        header.style.position = 'fixed'
         icon.style.color = 'white'
     }
 })
